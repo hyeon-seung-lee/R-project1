4a922bdcc366b848c5f971c6a68e8c8e29dee61c
lavistar <wila@tistory.com>
2019-10-18 18:18:12 +0900

    서울 지도 만들기 시작

--- seoulMap.r
+++ seoulMap.r
@@ -1,4 +1,19 @@
 install.packages('ggmap')
+install.packages('ggplot2')
+install.packages('raster')
+install.packages('rgeos')
+install.packages('maptools')
+install.packages('rgdal')
+
 library(ggmap)
+library(ggplot2)
+library(raster)
+library(rgeos)
+library(maptools)
+library(rgdal)
+
 geocode('Korea', source = 'google')
-geocode
+map <- shapefile('C:/dev/R-project1/maps/TL_SCCO_SIG.shp')
+setwd ('C:/dev/R-project1')
+
+# https://givitallugot.tistory.com/2
